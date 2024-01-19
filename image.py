711b5f08f1a52f148169648af02660c6766c8373
sufyaan <97327266+sufyn@users.noreply.github.com>
2024-01-19 14:58:15 +0530

    Update image.py

--- image.py
+++ image.py
@@ -54,76 +54,79 @@ st.set_page_config(page_title="IEEE CS PROMPATHON", page_icon="📜", layout="wi
 st.title("IEEE CS PROMPATHON", anchor=False)
 st.header("Compare Your Prompts with AI (OpenDalleV1.1 and all-MiniLM-L6-v2)", anchor=False)
 st.write('by sufyaan')
+tab1, tab2, tab3 = st.tabs(["Round 1", "Round 2", "Round 3"])
+with tab1:
+    # Input URL
+    st.divider()
+    st.write('ROUND 1')
+    prompt1 = st.text_input("Enter Your Prompt:", value="")
+    
+    if prompt1:
+        with st.status("Processing...", state="running", expanded=True) as status:
+            st.write("Generating Image ...")
+            img1 = image(prompt1)
+            # Download image
+            st.divider()
+            st.image(img1,width=800,caption=prompt1)
+            st.write("Checking Your Accuracy...")
+            accuracy1 = accuracy(prompt1)
+            status.update(label="Finished", state="complete")
+        # Show Summary
+        st.subheader("Accuracy:", anchor=False)
+        st.write(accuracy1)
+    
+    
+    st.divider()
+    st.header("Image to be generated", anchor=False)
+    st.image('astro.jpg',width=600)
 
-# Input URL
-st.divider()
-st.write('ROUND 1')
-prompt1 = st.text_input("Enter Your Prompt:", value="")
+with tab2:
 
-if prompt1:
-    with st.status("Processing...", state="running", expanded=True) as status:
-        st.write("Generating Image ...")
-        img1 = image(prompt1)
-        # Download image
-        st.divider()
-        st.image(img1,width=800,caption=prompt1)
-        st.write("Checking Your Accuracy...")
-        accuracy1 = accuracy(prompt1)
-        status.update(label="Finished", state="complete")
-    # Show Summary
-    st.subheader("Accuracy:", anchor=False)
-    st.write(accuracy1)
+    st.write('ROUND 2')
+    prompt2 = st.text_input("Enter Your Prompt:", value="")
+    
+    if prompt:
+        with st.status("Processing...", state="running", expanded=True) as status:
+            st.write("Generating Image ...")
+            img2 = image(prompt2)
+            # Download image
+            st.divider()
+            st.image(img2,width=800,caption=prompt2)
+            st.write("Checking Your Accuracy...")
+            accuracy2 = accuracy(prompt2)
+            status.update(label="Finished", state="complete")
+        # Show Summary
+        st.subheader("Accuracy:", anchor=False)
+        st.write(accuracy2)
+    
+    
+    st.divider()
+    st.header("Image to be generated", anchor=False)
+    st.image('astro.jpg',width=600)
 
+with tab3:
 
-st.divider()
-st.header("Image to be generated", anchor=False)
-st.image('astro.jpg',width=600)
-
-
-st.write('ROUND 2')
-prompt2 = st.text_input("Enter Your Prompt:", value="")
-
-if prompt:
-    with st.status("Processing...", state="running", expanded=True) as status:
-        st.write("Generating Image ...")
-        img2 = image(prompt2)
-        # Download image
-        st.divider()
-        st.image(img2,width=800,caption=prompt2)
-        st.write("Checking Your Accuracy...")
-        accuracy2 = accuracy(prompt2)
-        status.update(label="Finished", state="complete")
-    # Show Summary
-    st.subheader("Accuracy:", anchor=False)
-    st.write(accuracy2)
-
-
-st.divider()
-st.header("Image to be generated", anchor=False)
-st.image('astro.jpg',width=600)
-
-
-st.write('ROUND 3')
-prompt3 = st.text_input("Enter Your Prompt:", value="")
-
-if prompt:
-    with st.status("Processing...", state="running", expanded=True) as status:
-        st.write("Generating Image ...")
-        img3 = image(prompt3)
-        # Download image
-        st.divider()
-        st.image(img3,width=800,caption=prompt3)
-        st.write("Checking Your Accuracy...")
-        accuracy3 = accuracy(prompt3)
-        status.update(label="Finished", state="complete")
-    # Show Summary
-    st.subheader("Accuracy:", anchor=False)
-    st.write(accuracy3)
-
-
-st.divider()
-st.header("Image to be generated", anchor=False)
-st.image('astro.jpg',width=600)
+    st.write('ROUND 3')
+    prompt3 = st.text_input("Enter Your Prompt:", value="")
+    
+    if prompt:
+        with st.status("Processing...", state="running", expanded=True) as status:
+            st.write("Generating Image ...")
+            img3 = image(prompt3)
+            # Download image
+            st.divider()
+            st.image(img3,width=800,caption=prompt3)
+            st.write("Checking Your Accuracy...")
+            accuracy3 = accuracy(prompt3)
+            status.update(label="Finished", state="complete")
+        # Show Summary
+        st.subheader("Accuracy:", anchor=False)
+        st.write(accuracy3)
+    
+    
+    st.divider()
+    st.header("Image to be generated", anchor=False)
+    st.image('astro.jpg',width=600)
 
 st.divider()
 st.header("Demo with Prompts", anchor=False)
